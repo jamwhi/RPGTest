ad1b3c12683cf1e86505bb0503419702c7cc4872
jamwhi <jamesgeewhite@gmail.com>
2016-07-21 15:14:11 +1000

    Add falling effect, adjust ground textures, organise prefab folders

---
+++ Assets/3D Assets/Player/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public GameObject fallParticle;
+
+	private bool alive = true;
+	private Rigidbody rb;
+	private Vector3 startPos;
+	private MenuController menuController;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		menuController = GameObject.FindWithTag("UI").GetComponent<MenuController>();
+		startPos = transform.position;
+	}
+
+	void FixedUpdate () {
+		if (transform.position.y < -5f) {
+			return;
+		}
+
+		Vector3 force = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical") * speed);
+		rb.AddForce(force, ForceMode.Force);
+	}
+
+	void OnTriggerEnter() {
+		menuController.OpenShop();
+	}
+
+	void OnTriggerExit () {
+		menuController.CloseShop();
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (!alive) {
+			return;
+		}
+		if (col.collider.gameObject.name == "LowerGround") {
+			StartCoroutine(Fall());
+		}
+	}
+
+	IEnumerator Fall() {
+		alive = false;
+		GameObject fall = Instantiate(fallParticle, transform.position, Quaternion.identity) as GameObject;
+		fall.transform.parent = transform.parent;
+		this.enabled = false;
+		yield return new WaitForSeconds(3);
+		transform.position = startPos;
+		this.enabled = true;
+		alive = true;
+	}
+}
---
+++ Assets/KillAfterTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class KillAfterTime : MonoBehaviour {
+
+	public float time;
+
+	void Update () {
+		time -= Time.deltaTime;
+		if (time <= 0f) {
+			Destroy(gameObject);
+		}
+	}
+}
