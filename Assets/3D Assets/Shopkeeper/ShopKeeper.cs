3b7efe6d43167df9d83940f0d5969139e9582c57
jamwhi <jamesgeewhite@gmail.com>
2016-07-24 00:45:51 +1000

    remove debug messages on shopkeeper

--- Assets/3D Assets/Shopkeeper/ShopKeeper.cs
+++ Assets/3D Assets/Shopkeeper/ShopKeeper.cs
@@ -6,21 +6,18 @@ public class ShopKeeper : MonoBehaviour, IPointerClickHandler {
 	private bool playerNear = false;
 
 	public void OnPointerClick (PointerEventData eventData) {
-		Debug.Log("Clicked");
 		if (playerNear) {
 				AssetManager.GetInstance().menuController.OpenShop();
 		}
 	}
 
 	void OnTriggerEnter (Collider other) {
-		Debug.Log("Enter");
 		if (other.tag == "Player") {
 			playerNear = true;
 		}
 	}
 
 	void OnTriggerExit (Collider other) {
-		Debug.Log("Exit");
 		if (other.tag == "Player") {
 			playerNear = false;
 		}
